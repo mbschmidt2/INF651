e6502913cfbda3cd51f3e50d7c782de915fbdc44
mbschmidt2 <143021581+mbschmidt2@users.noreply.github.com>
2023-09-18 22:51:07 -0500

    Add files via upload

---
+++ oddeven.js
@@ -0,0 +1,14 @@
+let num = prompt("Enter a number");
+
+function oddeven(num) { 
+
+   if (num % 2 === 0) {
+    console.log("The number is even")
+   }else {
+            console.log("the number is odd")
+        }
+    
+   }
+
+
+oddeven(num);
